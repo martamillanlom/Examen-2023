e6a009cf0ff40411f9f11322d7a367ba2309a0b5
Marta Millan Lombraña <marta.millan@iesjoandaustria.org>
2023-01-12 14:47:59 +0100

    examen 2023

---
+++ app/src/main/java/com/example/examen2023/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.examen2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val bottomNav: BottomNavigationView = findViewById(R.id.main_menu)
+
+
+    }
+}
